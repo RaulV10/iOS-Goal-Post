49b6809aa09dfd83037d8ce8b0398e78476c78cd
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-24 17:10:03 -0600

    Build CreateGoalVC

---
+++ iOS-Goal-Post/Controller/CreateGoalVC.swift
@@ -0,0 +1,44 @@
+//
+//  CreateGoalVC.swift
+//  iOS-Goal-Post
+//
+//  Created by Raul Ernesto Villarreal Sigala on 7/24/18.
+//  Copyright © 2018 Raul Ernesto Villarreal Sigala. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var txtGoal: UITextView!
+    @IBOutlet weak var btnShortTerm: UIButton!
+    @IBOutlet weak var btnLongTerm: UIButton!
+    @IBOutlet weak var btnNext: UIButton!
+    
+    // Variables
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnShortTermWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func btnLongTermWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func btnNextWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func btnBackWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
